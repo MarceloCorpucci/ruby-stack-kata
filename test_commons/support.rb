d70f3f463555ffe5cfa2663351ca75fc9261889a
marcelo-corpucci <mcorpucci@velocitypartners.net>
2016-09-22 14:35:12 -0300

    url was changed to localhost.

--- test_commons/support.rb
+++ test_commons/support.rb
@@ -8,7 +8,7 @@ end
 
 module CleanUp
   def delete_entry
-  	@driver.navigate.to "https://effective-testing.herokuapp.com/admin"
+  	@driver.navigate.to "localhost:5000/admin"
   	@driver.find_element(:xpath, '/html/body/div/div[1]/div/ul[1]/li[2]/a').click
   	@driver.find_element(:xpath, '/html/body/div/table/tbody/tr[3]/td[2]/form/button/i').click
   	@driver.switch_to.alert.accept
